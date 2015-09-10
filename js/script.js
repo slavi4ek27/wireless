0ee01c77f025350040f947e03c1a1cb7e75994c5
slavi4ek27 <slavi4ek27@gmail.com>
2015-09-11 00:24:56 +0300

    added youtube video in fancybox popup

--- js/script.js
+++ js/script.js
@@ -5,4 +5,16 @@ $(document).ready(function(){
 });
 
 
-
+$(document).ready(function() {
+	$(".video").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: true,
+		closeClick	: true,
+		openEffect	: 'elastic',
+		closeEffect	: 'elastic'
+	});
+});
